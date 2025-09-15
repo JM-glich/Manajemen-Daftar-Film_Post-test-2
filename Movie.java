18780a8b96127549f6df631cd7cb8455d60301f1
Jemis Movid <jemismovid@gmail.com>
2025-09-16 00:03:16 +0800

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,66 @@
+package model;
+
+/**
+ * Model class untuk menyimpan atribut Movie.
+ */
+public class Movie {
+    // properties (access modifiers: private)
+    private int id;
+    private String title;
+    private String genre;
+    private int year;
+    private double rating; // 0.0 - 10.0
+
+    // constructor
+    public Movie(int id, String title, String genre, int year, double rating) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.year = year;
+        this.rating = rating;
+    }
+
+    // getters & setters (encapsulation)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ID:%d | %s (%d) | %s | Rating: %.1f", id, title, year, genre, rating);
+    }
+}
