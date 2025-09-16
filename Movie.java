90a8dd59156c97e69527a26439f904c236068b3c
Jemis Movid <jemismovid@gmail.com>
2025-09-16 10:28:59 +0800

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -24,37 +24,30 @@ public class Movie {
     public int getId() {
         return id;
     }
-
-    public void setId(int id) { this.id = id; }
-
+    public void setId(int id) { 
+        this.id = id; 
+    }
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getGenre() {
         return genre;
     }
-
     public void setGenre(String genre) {
         this.genre = genre;
     }
-
     public int getYear() {
         return year;
     }
-
     public void setYear(int year) {
         this.year = year;
     }
-
     public double getRating() {
         return rating;
     }
-
     public void setRating(double rating) {
         this.rating = rating;
     }
@@ -64,3 +57,4 @@ public class Movie {
         return String.format("ID:%d | %s (%d) | %s | Rating: %.1f", id, title, year, genre, rating);
     }
 }
+
